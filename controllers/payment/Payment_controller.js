6a8befe0dec795bfa975a0175949f559e35b90cb
M-Saffan75 <saffan.khan175@gmail.com>
2023-12-28 18:59:22 -0700

    payment column add

--- controllers/payment/Payment_controller.js
+++ controllers/payment/Payment_controller.js
@@ -6,7 +6,7 @@ const Payment_User = async (req, res) => {
     const { amount, currency } = req.body
 
     if (!amount || !currency) {
-        return res.status(403).json({ message: 'fields are found.', status: 'failed', code: 403 });
+        return res.status(403).json({ message: 'fields are required.', status: 'failed', code: 403 });
     }
 
     const customer = await stripe.customers.create();
@@ -41,20 +41,20 @@ const Subscription_Approve = async (req, res) => {
         const userId = req.user.id;
 
         if (!payment) {
-            return res.status(403).json({ message: 'fields are found.', status: 'failed', code: 403 });
+            return res.status(403).json({ message: 'fields are required.', status: 'failed', code: 403 });
         }
 
-        const updatedFeed = await User.findByIdAndUpdate(
+        const updatesubs = await User.findByIdAndUpdate(
             userId,
             { $set: { payment: payment } },
             { new: true }
         );
 
-        if (!updatedFeed) {
+        if (!updatesubs) {
             return res.status(404).json({ message: 'Feed not found.', status: 'failed', code: 404 });
         }
 
-        res.status(200).json({ message: 'Feed approved successfully', feed: updatedFeed, code: 200 });
+        res.status(200).json({ message: 'subscription approved successfully', subscription: updatesubs, code: 200 });
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: 'Internal Server Error', error: error.message, status: 'failed' });
