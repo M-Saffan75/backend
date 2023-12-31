3180579f99d0885905ec412ddb28cf8cd924f828
M-Saffan75 <saffan.khan175@gmail.com>
2023-12-30 23:59:25 -0700

    subscription time added

--- routes/apppayment/payment_routes.js
+++ routes/apppayment/payment_routes.js
@@ -35,7 +35,7 @@ payrouter.post('/decrease/payment/subscription', async (req, res) => {
         return res.status(200).json({ message: 'Decrement triggered successfully', userId, code: 200 , status:'success'});
     } catch (error) {
         console.error('Error triggering decrement:', error);
-        return res.status(500).json({ message: 'Error triggering decrement', error: error.message });
+        return res.status(500).json({ message: 'Error triggering decrement', error: error.message});
     }
 });
 
