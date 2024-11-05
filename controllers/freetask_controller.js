7ce3bb64f5d9531281a1c436e031a269bdcc0f98
M.Saffan Khan <92519898+M-Saffan75@users.noreply.github.com>
2024-11-05 23:48:14 +0500

    Update freetask_controller.js

--- controllers/freetask_controller.js
+++ controllers/freetask_controller.js
@@ -65,7 +65,7 @@ const Demo_Task_Create = async (req, res) => {
 const Demo_Get_Task = async (req, res) => {
     try {
         const tasks = await FreeTask.find();
-        return res.status(200).json({ message: 'Tasks Retrieved Successfully', tasks: tasks });
+        return res.status(200).json({ message: "Tasks Retrieved Successfully", tasks: tasks });
     } catch (error) {
         console.error(error);
         return res.status(500).json({ message: 'Internal Server Error' });
