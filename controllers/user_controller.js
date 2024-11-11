d745dd2cce16d0a00d069d97e8296348e5eb3544
M.Saffan Khan <92519898+M-Saffan75@users.noreply.github.com>
2024-11-11 23:24:22 +0500

    Update user_controller.js

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -352,7 +352,7 @@ const Update_Profile = async (req, res) => {
 const All_User = async (req, res) => {
     try {
         const userCount = await User.countDocuments();
-        res.status(200).json({ message: 'Total user count fetched successfully", count: userCount, code: 200 });
+        res.status(200).json({ message: "Total user count fetched successfully", count: userCount, code: 200 });
     } catch (error) {
         console.error('Error during user count retrieval:', error.message);
         res.status(500).json({ message: 'Internal Server Error', error: error.message, status: 'failed' });
